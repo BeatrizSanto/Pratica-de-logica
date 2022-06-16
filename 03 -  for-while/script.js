7304eccac33470028454375bd028b216e8531b37
Beatriz Santos <beatriz.santos19@yahoo.com.br>
2022-06-16 19:47:05 -0300

    g

---
+++ 03 -  for-while/script.js
@@ -0,0 +1,31 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(let number =1; number <10; number++){
+    console.log('resposta do 1º exercício: ',number);
+}
+console.log('-----------------------')
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let numero = 10;
+while(numero >=1){
+    console.log('resposta do 2º exercício: ',number);
+}
+console.log('-----------------------')
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(let numberImpar = 1; numberImpar <= 100; numberImpar+2){
+ console.log('resposta do 3º exercício: ',numberImpar);
+} 
+console.log('-----------------------')
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+let numeroPar = 0;
+while(numeroPar <=100){
+ console.log('resposta do 4º exercício: ',numberPar)
+ numberPar+=2;
+}
+console.log('-----------------------')
